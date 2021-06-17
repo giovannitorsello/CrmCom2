773711a579c8647a050ce4c79f5df9cfcdc53662
Giovanni Torsello <giovanni.torsello@gmail.com>
2021-06-17 10:51:38 +0200

    Remove the now ignored directory data_csv

--- data_csv/db_utility.sql
+++
@@ -1,2 +0,0 @@
-#create first administration account
-insert into users (username, password, role, status, email, firstname, lastname) values ('torsello', 'essequel', 'admin', 'active', 'giovanni.torsello@gmail.com', 'Giovanni', 'Torsello');
