fb6561d67c149d1617bc509e73ae834ee6060eca
Giovanni Torsello <giovanni.torsello@gmail.com>
2021-06-21 10:30:06 +0200

    Device sections is working

--- CrmCom2_Frontend/src/pages/DeviceCustomer.vue
+++ CrmCom2_Frontend/src/pages/DeviceCustomer.vue
@@ -1,118 +1,176 @@
 <template>
   <div id="device">
     <h6>
-      Gestione dispositivi      
+      Gestione dispositivi
     </h6>
     <p>
-      <strong>Cliente: {{selectedCustomer.id}}</strong>
-      <br/>
-      <strong>Contratto: {{selectedContract.id}}</strong>
+      <strong>Cliente: {{ customerDescription }}</strong>
+      <br />
+      <strong>Contratto: {{ selectedContract.id }}</strong>
     </p>
 
-    <img src="/img/actions/new.png"     @click="newDevice" style="width: 48px; height: 48px;" />
-    <img src="/img/actions/save.png"    @click="saveDevice" style="width: 48px; height: 48px;" />
-    <img src="/img/actions/delete.png"  @click="deleteDevice" style="width: 48px; height: 48px;" />
-    <img src="/img/actions/eye.svg"     @click="monitorDevice" style="width: 48px; height: 48px;" v-bind:class="monitorClassBackground"/>
-    <img src="/img/actions/exit.png"    @click="exit" style="width: 48px; height: 48px;" />
+    <img
+      src="/img/actions/new.png"
+      @click="newDevice"
+      style="width: 48px; height: 48px;"
+    />
+    <img
+      src="/img/actions/save.png"
+      @click="saveDevice"
+      style="width: 48px; height: 48px;"
+    />
+    <img
+      src="/img/actions/delete.png"
+      @click="deleteDevice"
+      style="width: 48px; height: 48px;"
+    />
+    <img
+      src="/img/actions/eye.svg"
+      @click="monitorDevice"
+      style="width: 48px; height: 48px;"
+      v-bind:class="monitorClassBackground"
+    />
+    <img
+      src="/img/actions/exit.png"
+      @click="exit"
+      style="width: 48px; height: 48px;"
+    />
 
     <ValidationObserver ref="FormDeviceCustomer">
-    <q-form ref="deviceForm" class="q-gutter-md">
-      <div class="row">
-        <div class="col">
-          <ValidationProvider name="Tecnologia" immediate rules="required" v-slot="{ errors }">
-            <q-select
+      <q-form ref="deviceForm" class="q-gutter-md">
+        <div class="row">
+          <div class="col">
+            <ValidationProvider
+              name="Tecnologia"
+              immediate
+              rules="required"
+              v-slot="{ errors }"
+            >
+              <q-select
                 filled
                 label="Asset"
                 @input="changeAsset"
                 :options="selectAssetOptions"
                 v-model="selectedAsset"
-                map-options                  
-                option-label="label" 
+                map-options
+                option-label="label"
                 option-value="value"
-            />            
-            <span class="error">{{ errors[0] }}</span>
-          </ValidationProvider>
+              />
+              <span class="error">{{ errors[0] }}</span>
+            </ValidationProvider>
+          </div>
         </div>
-      </div>
-      <div class="row">
-        <div class="col">
-          <ValidationProvider name="Descrizione" immediate rules="required|alpha" v-slot="{ errors }">
-            <q-input label="Descrizione" v-model="selectedDevice.description" />
-            <span class="error">{{ errors[0] }}</span>
-          </ValidationProvider>
+        <div class="row">
+          <div class="col">
+            <ValidationProvider
+              name="Descrizione"
+              immediate
+              rules="required"
+              v-slot="{ errors }"
+            >
+              <q-input
+                label="Descrizione"
+                v-model="selectedDevice.description"
+              />
+              <span class="error">{{ errors[0] }}</span>
+            </ValidationProvider>
+          </div>
+          <div class="col">
+            <ValidationProvider
+              name="Tipo"
+              immediate
+              rules="required"
+              v-slot="{ errors }"
+            >
+              <q-input label="Tipo" v-model="selectedDevice.type" />
+              <span class="error">{{ errors[0] }}</span>
+            </ValidationProvider>
+          </div>
+          <div class="col"></div>
+          <div class="col">
+            <ValidationProvider
+              name="Produttore"
+              immediate
+              rules="required|alpha_spaces"
+              v-slot="{ errors }"
+            >
+              <q-input label="Produttore" v-model="selectedDevice.vendor" />
+              <span class="error">{{ errors[0] }}</span>
+            </ValidationProvider>
+          </div>
+          <div class="col">
+            <ValidationProvider
+              name="Modello"
+              immediate
+              rules="required|alpha"
+              v-slot="{ errors }"
+            >
+              <q-input label="Modello" v-model="selectedDevice.model" />
+              <span class="error">{{ errors[0] }}</span>
+            </ValidationProvider>
+          </div>
         </div>
-        <div class="col">
-          <ValidationProvider name="Tipo" immediate rules="required|alpha" v-slot="{ errors }">
-            <q-input label="Tipo" v-model="selectedDevice.type" />
-            <span class="error">{{ errors[0] }}</span>
-          </ValidationProvider>
+        <div class="row">
+          <div class="col">
+            <ValidationProvider
+              name="Indirizzo MAC"
+              immediate
+              rules="required|mac"
+              v-slot="{ errors }"
+            >
+              <q-input label="MAC" v-model="selectedDevice.mac" />
+              <span class="error">{{ errors[0] }}</span>
+            </ValidationProvider>
+          </div>
+          <div class="col">
+            <ValidationProvider
+              name="Indirizzo IP"
+              immediate
+              rules="required|ipv4"
+              v-slot="{ errors }"
+            >
+              <q-input label="ipv4" v-model="selectedDevice.ipv4" />
+              <span class="error">{{ errors[0] }}</span>
+            </ValidationProvider>
+          </div>
+          <div class="col">
+            <ValidationProvider
+              name="Tecnologia"
+              immediate
+              rules="ipv6"
+              v-slot="{ errors }"
+            >
+              <q-input label="ipv6" v-model="selectedDevice.ipv6" />
+              <span class="error">{{ errors[0] }}</span>
+            </ValidationProvider>
+          </div>
         </div>
-        <div class="col"></div>
-        <div class="col">
-          <ValidationProvider name="Produttore" immediate rules="required|alpha_spaces" v-slot="{ errors }">
-            <q-input label="Produttore" v-model="selectedDevice.vendor" />
-            <span class="error">{{ errors[0] }}</span>
-          </ValidationProvider>
+        <div class="row">
+          <div class="col">
+            <q-input
+              label="Note sul dispositivo"
+              v-model="selectedDevice.note"
+              type="textarea"
+              rows="5"
+              cols="20"
+            />
+          </div>
         </div>
-        <div class="col">
-          <ValidationProvider name="Modello" immediate rules="required|alpha" v-slot="{ errors }">
-            <q-input label="Modello" v-model="selectedDevice.model" />
-            <span class="error">{{ errors[0] }}</span>
-          </ValidationProvider>          
-        </div>
-      </div>
-      <div class="row">
-        <div class="col">
-          <ValidationProvider name="Tecnologia" immediate rules="mac" v-slot="{ errors }">
-            <q-input label="MAC" v-model="selectedDevice.mac" />
-            <span class="error">{{ errors[0] }}</span>
-          </ValidationProvider>
-        </div>
-        <div class="col">
-          <ValidationProvider name="Tecnologia" immediate rules="required|ipv4" v-slot="{ errors }">
-            <q-input label="ipv4" v-model="selectedDevice.ipv4" />
-            <span class="error">{{ errors[0] }}</span>
-          </ValidationProvider>          
-        </div>
-        <div class="col">
-          <ValidationProvider name="Tecnologia" immediate rules="ipv6" v-slot="{ errors }">
-            <q-input label="ipv6" v-model="selectedDevice.ipv6" />
-            <span class="error">{{ errors[0] }}</span>
-          </ValidationProvider>          
-        </div>
-      </div>
-      <div class="row">
-        <div class="col">
-          <q-input
-            label="Note sul dispositivo"
-            v-model="selectedDevice.note"
-            type="textarea"
-            rows="5"
-            cols="20"
-          />
-        </div>
-      </div>
-    </q-form>
+      </q-form>
     </ValidationObserver>
 
     <hr class="separator" />
-    <template v-if="selectedDevice">
-      <h6>Parametri generali</h6>
+    <div>
       <div id="deviceParametersPrimitives" v-if="selectedDevice.objData">
+        <h6>Parametri generali</h6>
         <div
           class="row"
           v-for="(primitive, primitiveName) in objDataPrimitives"
           v-bind:key="primitive"
         >
           <div class="col">
-            <label
-              v-bind:for="primitiveName"
-              class="form-control form-control-user"
-            >{{primitiveName}}</label>
-          </div>
-          <div class="col">
-            <input
-              class="form-control form-control-user"
+            <q-input
+              :label="primitiveName"
               v-bind:id="primitiveName"
               v-model="selectedDevice.objData[primitiveName]"
             />
@@ -121,27 +179,23 @@
       </div>
 
       <hr class="separator" />
-      <h6>Sezioni specifiche</h6>
       <div id="deviceParametersSections" v-if="selectedDevice.objData">
+        <h6>Sezioni specifiche</h6>
         <div
           v-for="(section, sectionName, indexSection) in objDataSection"
           v-bind:key="indexSection"
         >
           <h6>
-            <p align="left">{{ sectionName}}</p>
+            <p align="left">{{ sectionName }}</p>
           </h6>
-          <div class="row">
-            <div
-              v-for="(devProperty,devPropertyName, indexProperty) in section"
-              v-bind:key="indexProperty"
-            >
+          <div
+            v-for="(devProperty, devPropertyName, indexProperty) in section"
+            v-bind:key="indexProperty"
+          >
+            <div class="row">
               <div class="col">
-                <label
-                  v-bind:for="devPropertyName"
-                  class="form-control form-control-user"
-                >{{devPropertyName}}</label>
-                <input
-                  class="form-control form-control-user"
+                <q-input
+                  :label="devPropertyName"
                   v-bind:id="devPropertyName"
                   v-model="selectedDevice.objData[sectionName][devPropertyName]"
                 />
@@ -151,7 +205,7 @@
           <hr class="separator" />
         </div>
       </div>
-    </template>
+    </div>
   </div>
 </template>
 
@@ -168,15 +222,14 @@ export default {
       selectedCustomer: {},
       selectedContract: {},
       selectedAsset: {},
-      customerDescription: "",
       contractDescription: "",
       selectAssetOptions: [],
       companyassets: companyassetsJson,
       companyasset: {},
-      selectedDevice: {},  
+      selectedDevice: {},
       devices: {},
       objDataSection: {},
-      objDataPrimitives: {},                    
+      objDataPrimitives: {},
     }
   },
   components: {
@@ -186,7 +239,7 @@ export default {
   methods: {
     populateSelectAsset() {
       this.companyassets.forEach(element => {
-        this.selectAssetOptions.push({label: element.company+" -- "+element.techasset, value: element});        
+        this.selectAssetOptions.push({label: element.company+" -- "+element.techasset, value: element});
       });
     },
     changeAsset() {
@@ -194,36 +247,49 @@ export default {
       this.objDataPrimitives=[];
       this.selectedDevice.objData={};
       this.companyasset=this.selectedAsset.value;
-      var res=Object.assign(this.selectedDevice.objData, this.companyasset.propDeviceStructure)      
-      this.$store.commit("changeDeviceCustomer", this.selectedDevice); 
-      this.selectedDevice.id="";
-      this.parseDeviceObjData(); 
-    },       
+      var res=Object.assign(this.selectedDevice.objData, this.companyasset.propDeviceStructure)
+      this.$store.commit("changeDeviceCustomer", this.selectedDevice);
+      this.parseDeviceObjData();
+    },
     parseDeviceObjData: function() {
-      //if(this.selectedDevice && this.selectedDevice.id && this.selectedDevice.objData)
-      for (const sectionName in this.selectedDevice.objData) {        
+      this.objDataSection={};
+      this.objDataPrimitives={};
+      if(this.selectedDevice && this.selectedDevice.objData)
+      for (const sectionName in this.selectedDevice.objData) {
         if(typeof(this.selectedDevice.objData[sectionName])==="object") {
           console.log("Found object: "+sectionName);
           this.objDataSection[sectionName]=this.selectedDevice.objData[sectionName];
         }
         else {
           console.log("Found simple data: "+sectionName);
-          this.objDataPrimitives[sectionName]=this.selectedDevice.objData[sectionName];          
+          this.objDataPrimitives[sectionName]=this.selectedDevice.objData[sectionName];
         }
-      }              
+      }
     },
     getDeviceData: function() {
-      this.selectedDevice= Object.assign({}, this.$store.state.deviceCustomer);  
+      this.selectedDevice= Object.assign({}, this.$store.state.deviceCustomer);
       if(this.selectedDevice===null) this.selectedDevice={objData: {}};
       if(this.selectedDevice.objData===null) this.selectedDevice={objData: {}};
       this.$store.commit("changeDeviceCustomer", this.selectedDevice);
-      
-      //init correct companyasset in selection        
+
+      //init correct companyasset in selection
       this.parseDeviceObjData();
+      //compute selectedAsset
+      const companyasset=this.selectedDevice.companyasset;
+      const techasset=this.selectedDevice.techasset;
+
+      console.log(companyasset);
+      console.log(techasset);
+      console.log(this.selectAssetOptions);
+      this.selectedAsset=this.selectAssetOptions.find((asset) => {
+        return ((companyasset===asset.value.company) &&
+        (techasset===asset.value.techasset));
+      })
+      console.log(this.selectedAsset);
     },
     newDevice() {
-      this.selectedDevice={};        
-      this.$store.commit("changeDeviceCustomer", this.selectedDevice);      
+      this.selectedDevice={};
+      this.$store.commit("changeDeviceCustomer", this.selectedDevice);
       this.makeToast("Configura un nuovo dispositivo");
     },
     saveDevice: function() {
@@ -232,14 +298,14 @@ export default {
       this.selectedDevice.state="active";
       this.selectedDevice.contractId=this.contract.id;
       this.$axios.post('/adminarea/deviceCustomer/update', {device: this.selectedDevice})
-        .then((response) => {       
-              if (response.data.status === "OK") {                  
-                  this.selectedDevice = response.data.deviceCustomer;                    
-                  this.$store.commit("changeDeviceCustomer", this.selectedDevice);      
-                  this.makeToast(response.data.msg);                              
-              }                                     
+        .then((response) => {
+              if (response.data.status === "OK") {
+                  this.selectedDevice = response.data.deviceCustomer;
+                  this.$store.commit("changeDeviceCustomer", this.selectedDevice);
+                  this.makeToast(response.data.msg);
+              }
           })
-          .catch(error => {                              
+          .catch(error => {
               console.log(error);
           });
     },
@@ -247,15 +313,15 @@ export default {
       const isConfirmed = confirm("Confermi la cancellazione?");
       if(isConfirmed) {
       this.$axios.post('/adminarea/deviceCustomer/delete', {device: this.selectedDevice})
-        .then(response => {                
-              if (response.data.status === "OK") {                  
-                  this.selectedDevice = response.data.deviceCustomer;  
-                  this.$store.commit("changeDeviceCustomer", this.selectedDevice);      
-                  this.makeToast(response.data.msg); 
-                  this.exit();            
-              }                                     
+        .then(response => {
+              if (response.data.status === "OK") {
+                  this.selectedDevice = response.data.deviceCustomer;
+                  this.$store.commit("changeDeviceCustomer", this.selectedDevice);
+                  this.makeToast(response.data.msg);
+                  this.exit();
+              }
           })
-          .catch(error => {                              
+          .catch(error => {
               console.log(error);
           });
       }
@@ -264,19 +330,19 @@ export default {
       this.$router.push("/Contract");
     },
     monitorDevice() {
-      this.selectedDevice=Object.assign({}, this.$store.state.deviceCustomer)      
+      this.selectedDevice=Object.assign({}, this.$store.state.deviceCustomer)
       if (this.selectedDevice) {
         if (this.selectedDevice.monitored !== "monitored") this.selectedDevice.monitored = "monitored";
-        else this.selectedDevice.monitored = "";             
+        else this.selectedDevice.monitored = "";
         this.saveDevice();
       }
     },
-    makeToast(string) {        
+    makeToast(string) {
         this.$q.notify({color: 'green-4', textColor: 'white', icon: 'info', message: string});
       }
-    }, 
+    },
   computed: mapState({
-    user: 'user', 
+    user: 'user',
     customer: 'customer',
     contract: 'contract',
     service: 'service',
@@ -285,7 +351,7 @@ export default {
       const customer=this.customer;
       if(customer.vatcode && customer.vatcode!=="")
         return customer.company;
-      else 
+      else
         return customer.lastname+" "+customer.firstname;
     },
     monitorClassBackground() {
@@ -296,12 +362,11 @@ export default {
     this.selectedCustomer=Object.assign({}, this.$store.state.customer);
     this.selectedContract=Object.assign({}, this.$store.state.contract);
     this.populateSelectAsset();
-    this.getDeviceData();       
+    this.getDeviceData();
   },
     beforeRouteEnter(to, from, next) {
     var currentUser = Store.state.user;
-    console.log(currentUser);
-    if ((currentUser.role === "admin") || 
+    if ((currentUser.role === "admin") ||
         (currentUser.role === "manager") ||
         (currentUser.role === "technician"))  next();
     else next("/Login");
